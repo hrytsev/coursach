a219c1c04d6f729c86e983621adac8fced844108
Hrytsev <hrytsevivan@gmail.com>
2024-04-18 22:27:05 +0300

    refactored two supervisor methods

--- yt_DesignUI/Models/Supervisor.cs
+++ yt_DesignUI/Models/Supervisor.cs
@@ -13,8 +13,22 @@ namespace yt_DesignUI.Models
                 : base(isChecked, inbox, workHours, position, working, name, birthday, id, rate,lastStartTime)
         {
         }
-
-        
+        public void addNew(bool isAdmin, bool isChecked, List<string> inbox, float workHours, string position, bool working, string name, DateTime birthday, int id, float rate, DateTime lastStartTime, Enterprise currentEnterprise)
+        {
+            if (isAdmin)
+            {
+                Supervisor super = new Supervisor(isChecked, inbox, workHours, position, working, name, birthday, id, rate, lastStartTime);
+                currentEnterprise.addToWorkers(super);
+            }else
+            {
+                Employee employee = new Employee(isChecked, inbox, workHours, position, working, name, birthday, id, rate, lastStartTime);
+                currentEnterprise.addToWorkers(employee);
+            }
+        }
+        public void remove(Enterprise currentEnterpise,int index)
+        {
+            currentEnterpise.Employeers.RemoveAt(index);
+        }
         public void editEmployye(Employee currentWorker,string newName,string newPosition, float newRate,int newId)
         {
             currentWorker.changeName(newName);
